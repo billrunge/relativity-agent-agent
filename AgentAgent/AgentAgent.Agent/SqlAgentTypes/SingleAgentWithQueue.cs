0db422241a8e59486a06cf454c3bec8878b411ca
billrunge <billrunge@gmail.com>
2018-10-11 22:58:43 -0500

    Began work on SingleAgentWithQueue logic.

--- AgentAgent/AgentAgent.Agent/SqlAgentTypes/SingleAgentWithQueue.cs
+++ AgentAgent/AgentAgent.Agent/SqlAgentTypes/SingleAgentWithQueue.cs
@@ -1,9 +1,29 @@
-﻿namespace AgentAgent.Agent.SqlAgentTypes
+﻿using Relativity.API;
+
+namespace AgentAgent.Agent.SqlAgentTypes
 {
     class SingleAgentWithQueue
     {
-        public SingleAgentWithQueue()
+        private readonly IDBContext _eddsDbContext;
+        private readonly IEnvironmentHelper _environment;
+        private readonly string _eddsQueueTable;
+
+        public SingleAgentWithQueue(IDBContext eddsDbContext, IEnvironmentHelper environment, string eddsQueuTable)
         {
+            _eddsDbContext = eddsDbContext;
+            _environment = environment;
+
+
+        }
+
+        public int GetAgentsDesiredList()
+        {
+            string SQL = string.Format(@"
+                SELECT Count(*) 
+                FROM   [{0}] T 
+                       INNER JOIN [Case] C WITH(NOLOCK) 
+                               ON T.[{1}] = C.[ArtifactID] 
+                WHERE  C.[ResourceGroupArtifactID] = @ResourceGroupArtifactID", );
 
         }
     }
